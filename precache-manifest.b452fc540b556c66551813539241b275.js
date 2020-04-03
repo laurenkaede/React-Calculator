1a82c9cefe33ae5ffe758d6d36fb4df346c3db00
Lauren Moor <laurenkaede@gmail.com>
2020-04-03 16:35:36 +0100

    Updates

---
+++ precache-manifest.b452fc540b556c66551813539241b275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e933c92da61b3d3a138f318ae709cfb",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "dc41adcaaffe52e122a8",
+    "url": "/React-Calculator/static/css/main.2bf4b137.chunk.css"
+  },
+  {
+    "revision": "b311885ebfb2aa3eacd0",
+    "url": "/React-Calculator/static/js/2.7e49c228.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.7e49c228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc41adcaaffe52e122a8",
+    "url": "/React-Calculator/static/js/main.dc4686ca.chunk.js"
+  },
+  {
+    "revision": "79e53d394c71c17655ae",
+    "url": "/React-Calculator/static/js/runtime-main.e35d6ec7.js"
+  }
+]);
